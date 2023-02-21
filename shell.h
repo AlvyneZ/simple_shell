b07e2c6899e77637dfb021db68ea32e7de5e1a75
mwogoi <jinaromwogoi@gmail.com>
2023-02-21 10:29:41 -0800

    Prototype functions

--- shell.h
+++ shell.h
@@ -10,6 +10,7 @@
  * @cur_cmd_sz: Size of current command buffer
  * @cur_cmd_len: current command data length
  * @tok_cmd: Tokenized current command
+ * @envp: Array of strings of the environment
  *
  * Description: This struct is passed to most functions of the program as
  *  an alternative to global variables
@@ -21,11 +22,26 @@ struct shell_sts_s
 	size_t cur_cmd_sz;
 	ssize_t cur_cmd_len;
 	char **tok_cmd;
+	char **envp;
 };
 
 typedef struct shell_sts_s shell_sts_t;
 
+/* Defined in str_functions.c */
+int _strlen(char *s);
+char *_strchr(char *s, char c);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+
+/* Defined in input.c */
 void clear_cmd_buffer(shell_sts_t *sts);
 int get_command(shell_sts_t *sts);
+
+/* Defined in tokeniser.c */
+void clear_tok_cmd(shell_sts_t *sts);
+void tokenise_cmd(shell_sts_t *sts);
+
+/* Defined in execution.c */
+void execute_cmd(shell_sts_t *sts);
 
 #endif /* SHELL_HEADER_INCLUDE_ */
