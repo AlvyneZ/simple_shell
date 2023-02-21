f1c63ed014eb3aff103729db003677d54199d228
mwogoi <jinaromwogoi@gmail.com>
2023-02-21 10:31:53 -0800

    Gets the length of a string

---
+++ str_functions.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+#include <stdlib.h>
+
+/**
+* _strlen - Gets the length of a string
+ * @s: String whose length is required
+ * Return: Length of string s
+ */
+int _strlen(char *s)
+{
+	int ind;
+
+	for (ind = 0; (*(s + ind) != '\0'); ind++)
+	;
+	return (ind);
+}
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: String to be searched through
+ * @c: Character to be searched for
+ * Return: Pointer to first occurence of c or
+ *  NULL if c is absent
+ */
+char *_strchr(char *s, char c)
+{
+	char *sCurs;
+
+	sCurs = s;
+	for (; (*sCurs != '\0'); sCurs++)
+	{
+		if (*sCurs == c)
+			return (sCurs);
+	}
+	if (c == '\0')
+		return (sCurs);
+	return (NULL);
+}
+
+/**
+* _strcpy - Copies a string to another string
+ * @dest: Destination to paste string
+ * @src: Source string to copy from
+ * Return: destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *destCurs, *srcCurs;
+
+	srcCurs = src;
+	destCurs = dest;
+	for (; (*srcCurs != '\0'); srcCurs++, destCurs++)
+		*destCurs = *srcCurs;
+	*destCurs = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strdup - Duplicates a string
+ * @str: String to be duplicated
+ * Return: a pointer to the duplicated string, or NULL if
+ *  it fails or provided string is NULL
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+	dup = malloc(_strlen(str) + 1);
+	if (dup != NULL)
+		return (_strcpy(dup, str));
+	return (NULL);
+}
